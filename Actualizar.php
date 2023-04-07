81cce5d5bd99a16a5d7900cd3cdaef674fa2c372
Moisés David Ramón Esteban <masemoyv1@gmail.com>
2023-04-06 19:02:54 -0600

    Create Actualizar.php
    Ejemplo de archivo en php

---
+++ Actualizar.php
@@ -0,0 +1,29 @@
+
+<?php
+$x=1;
+?>
+<?php while($x < 11 ): ?>
+    <ul>Elemento <?php echo $x ?> </ul> 
+<?php $x++; endwhile ?>
+
+
+<ul>
+<?php
+$x=1;
+?>
+<?php while($x < 11 ): ?>
+    <li>Elemento <?php echo $x ?> </li> 
+<?php $x++; endwhile ?>
+
+</ul>
+
+
+<ol>
+<?php
+$x=1;
+?>
+<?php while($x < 11 ): ?>
+    <li>Elemento </li> 
+<?php $x++; endwhile ?>
+
+</ol>
